2ad057b7cd56ce213c48910ac86c727b54860f91
LestathRiveraD <cesarrivera0404@gmail.com>
2026-03-29 15:03:56 -0700

    oops 3

--- Implementation/Div2-1082-B.cpp
+++ Implementation/Div2-1082-B.cpp
@@ -7,26 +7,26 @@ using namespace std;
 
 void solve()
 {
-	int n, res = 0, cnt = 0;
+    /*
+        1. Is it dp?
+        2. How can I solve the ith subproblem?
+        3. Solve problem for case i, 1 <= i <= n
+        4. Find recurrence: res = (visited[i] ? res : res + 1)
+        4.1 DP will always find the correct solution because at every
+        point we're either discarding the current term and keeping the
+        previous one, or we do not discard it. In that case the solution
+        is always the previous one plus 1.
+    */
+    int n, res = 0;
 	cin >> n;
-	vi p(n);
-	for (int i = 0; i < n; i++)
-		cin >> p[i];
-		
-	vector<int> seen(n + 2, 0);
-	
-	for (int i = 0; i < n; i++) {
-	    if (p[i] > i + 1) {
-	        seen[p[i]]++;
-	    }
+	vector<int> v(n + 1), visited(n + 1, 0);
+	for (int i = 1; i <= n;i++)
+		cin >> v[i];
+	for (int i = 1; i <= n; i++)
+	{
+		res = (visited[i] == 1 ? res : res + 1);
+		visited[v[i]] = 1;
 	}
-	for (int i = 1; i <= n; i++) {
-	    if (seen[i]) {
-	        res = max(res, i - 1 - cnt);
-	        cnt++;
-	    }
-	}
-	res = max(res, n - cnt);
 	cout << res << '\n';
 }
 
